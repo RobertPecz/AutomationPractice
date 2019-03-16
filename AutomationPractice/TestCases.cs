394d561dee6d644a61069df85bc2f2f7e03a5323
Robert Pecz <rob.pecz@gmail.com>
2019-03-16 13:59:36 +0100

    Initial commit

---
+++ AutomationPractice/TestCases.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationPractice
+{
+    [TestFixture]
+    public class TestCases
+    {
+        [SetUp]
+        public void Instantiation()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https:\\google.com");
+        }
+        [Test]
+        public void Foobar() { }
+    }
+}
